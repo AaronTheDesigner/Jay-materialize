9d2ab7e54ec897542c9dca268c8fd1717bc8bc04
AaronTheDesigner <flux.charlotte@gmail.com>
2018-07-02 18:40:10 -0400

    repository commit

---
+++ app.js
@@ -0,0 +1,36 @@
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});    
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+})
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
+
+$('.fixed-action-btn').floatingActionButton({
+  toolbarEnabled: true
+});
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
